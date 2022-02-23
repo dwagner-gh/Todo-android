66c4caed4b72b1b72ea15b4f411d9868764df35d
dwagner <dennis.wagner@tutamail.com>
2022-02-23 11:07:25 +0100

    schedule background task for importing todo items

--- app/src/main/java/com/example/todo/ToDoApp.kt
+++ app/src/main/java/com/example/todo/ToDoApp.kt
@@ -2,10 +2,8 @@ package com.example.todo
 
 import android.app.Application
 import android.text.format.DateUtils
-import com.example.todo.repo.PrefsRepository
-import com.example.todo.repo.ToDoDatabase
-import com.example.todo.repo.ToDoRemoteDataSource
-import com.example.todo.repo.ToDoRepository
+import androidx.work.*
+import com.example.todo.repo.*
 import com.example.todo.report.RosterReport
 import com.example.todo.ui.SingleModelViewModel
 import com.example.todo.ui.roster.RosterViewModel
@@ -13,18 +11,25 @@ import com.github.jknack.handlebars.Handlebars
 import com.github.jknack.handlebars.Helper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import java.time.Instant
+import java.util.concurrent.TimeUnit
 
-class ToDoApp : Application() {
+private const val TAG_IMPORT_WORK = "doPeriodicImport"
+
+class ToDoApp : Application(), KoinComponent {
     private val koinModule = module {
         single(named("appScope")) { CoroutineScope(SupervisorJob()) }
         single { ToDoDatabase.newInstance(androidContext()) }
@@ -73,6 +78,42 @@ class ToDoApp : Application() {
             // https://github.com/InsertKoinIO/koin/issues/1188#issuecomment-970240532
             androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             modules(koinModule)
+        }
+        scheduleWork()
+    }
+
+    private fun scheduleWork() {
+        val prefs: PrefsRepository by inject()
+        val appScope: CoroutineScope by inject(named("appScope"))
+        val workManager = WorkManager.getInstance(this)
+
+        appScope.launch {
+            // while app is running, listen for changes to the import periodically option
+            prefs.observeImportChanges().collect {
+                if (it) {
+                    // what is needed by the worker
+                    val constraints = Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.CONNECTED)
+                        .build()
+
+                    // defines what and when it should be done
+                    val request =
+                        PeriodicWorkRequestBuilder<ImportWorker>(15, TimeUnit.MINUTES)
+                            .setConstraints(constraints)
+                            .addTag(TAG_IMPORT_WORK)
+                            .build()
+
+                    // actually scheduling the work
+                    // (unique means, if work is already present, act according to given policy)
+                    workManager.enqueueUniquePeriodicWork(
+                        TAG_IMPORT_WORK,
+                        ExistingPeriodicWorkPolicy.REPLACE,
+                        request
+                    )
+                } else {
+                    workManager.cancelAllWorkByTag(TAG_IMPORT_WORK)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/todo/repo/PrefsRepository.kt
+++ app/src/main/java/com/example/todo/repo/PrefsRepository.kt
@@ -1,9 +1,12 @@
 package com.example.todo.repo
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import com.example.todo.R
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.withContext
 
 class PrefsRepository(context: Context) {
@@ -11,8 +14,20 @@ class PrefsRepository(context: Context) {
     private val webServiceUrlKey = context.getString(R.string.web_service_url_key)
     private val defaultWebServiceUrl =
         context.getString(R.string.web_service_url_default)
+    private val importPeriodicKey = context.getString(R.string.import_periodic_key)
 
     suspend fun loadWebServiceUrl(): String = withContext(Dispatchers.IO) {
         prefs.getString(webServiceUrlKey, defaultWebServiceUrl) ?: defaultWebServiceUrl
+    }
+
+    // creates a flow that can be collected
+    fun observeImportChanges() = channelFlow {
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+            if (importPeriodicKey == key) {
+                trySend(prefs.getBoolean(importPeriodicKey, false)).isSuccess
+            }
+        }
+        prefs.registerOnSharedPreferenceChangeListener(listener)
+        awaitClose { prefs.unregisterOnSharedPreferenceChangeListener(listener) }
     }
 }
